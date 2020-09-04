e27743a5dee53dda099699af720f7293b0874684
Remi de Boer <r.d.d.de.boer@hva.nl>
2020-09-04 17:43:11 +0200

    Added SnackBar

--- app/src/main/java/com/remideboer/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/remideboer/madlevel2example/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,13 +27,18 @@ class MainActivity : AppCompatActivity() {
         // we willen toch ergens op kunnen klikken en dat het iets leuks doet
         btnAddReminder.setOnClickListener {
             // eerst valideren, graag geen lege strings in de lijst
-            etReminder.text.toString().takeIf { it.isNotBlank() }
-                ?.let {// isNotBlank geeft een null terug als wel een blank string, daar kunnen we gebruik van maken met een safe call
+            // beetje spelen met de Kotlin functies om te zien wat mogelijk is ipv een normale if
+            // en direct String te gebruiken ipv nogmaals uit etReminder halen
+            // niet dat het er perse sneller of leesbaarder van wordt.
+            etReminder.text.toString().takeIf { it.isNotBlank() } ?.let {// isNotBlank geeft een null terug als wel een blank String, daar kunnen we gebruik van maken met een safe call
                     list.add(Reminder(it))
                     // trigger update, een standaard methode van recyclerview
                     viewAdapter.notifyDataSetChanged()
-                }
-            Log.d("MainActivity", "onClick: ${list.size}, $list")
+                } ?: run {// Elvis operator gaat werken als er NullPointer uit komt
+                Snackbar.make(etReminder, getString(R.string.et_blank_error), Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+            Log.d("MainActivity", "onClick: ${list.size}, $list") // here to show list content
         }
     }
 }
